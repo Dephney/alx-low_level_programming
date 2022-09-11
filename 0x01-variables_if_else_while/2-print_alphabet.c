641a04420c90e058c2167d42005a162d001503e7
Dephney <mmagotsheung@gmail.com>
2022-09-11 06:55:24 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * description:print the alphabets in lowercase
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha == 'z'; alpha++)
+		printf("alpha");
+
+	return (0);
+}
+
