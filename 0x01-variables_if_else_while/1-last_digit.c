79b28e0e91d10153aa4185fff569f1e096740240
Dephney <mmagotsheung@gmail.com>
2022-09-12 16:37:08 -0700

    correct output

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,8 @@ int main(void)
 
 	srand(time(0));
 
-	n = rand() - RAND_MAX;
+	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-
-	if (last_digit < 0)
-		last_digit = -(last_digit);
 
 	if (last_digit == 0)
 		printf("%s %d is %d and is zero\n ", str, n, last_digit);
