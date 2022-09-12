d4f5bcc4f74f09e9fa7e2c1ef2adf8e1d86f43fb
Dephney <mmagotsheung@gmail.com>
2022-09-12 15:18:28 -0700

    added if statement

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,13 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num == '9')
+			break;
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
