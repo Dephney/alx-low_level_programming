0c2a668cea4f695a1dfa8b2bdd98d500d0562c5f
Dephney <mmagotsheung@gmail.com>
2022-09-13 06:48:11 -0700

    remove for loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,20 @@
 #include "main.h"
 
 /**
- * main - print _putchar
+ * main - Entry point
  *
  * Return - Always  (Success)
  */
 int main(void)
 {
-	char str[8] = "_putchar";
-	for (int i = 0; i < 8; i++)
-	{
-		char c = str[i];
-		_putchar(c);
-	}
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
