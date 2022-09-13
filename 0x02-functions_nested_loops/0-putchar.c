09c44a7a0312cf93b7512ed82b558baad6c6e5ec
Dephney <mmagotsheung@gmail.com>
2022-09-13 05:00:54 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return - Always  (Success)
+ */
+
+int main(void)
+{
+	char p[] = "_putchar";
+
+	_putchar(p);
+	return ();
+}
