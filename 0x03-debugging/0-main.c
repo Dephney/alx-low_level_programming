757b0a22eaa247d2c686c20cd22e757a01cf368b
Dephney <mmagotsheung@gmail.com>
2022-09-15 12:51:51 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  test that the function positive_or_negative()
+ * gives the correct output when given a case of 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+}
