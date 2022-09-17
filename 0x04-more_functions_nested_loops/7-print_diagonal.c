b7549eeef8ba7481a0e29b9b8a6457e847dea1d0
Dephney <mmagotsheung@gmail.com>
2022-09-16 19:41:12 -0700

    correct for loop

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,9 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j <= i - 1; j++)
+			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 					_putchar('\\');
