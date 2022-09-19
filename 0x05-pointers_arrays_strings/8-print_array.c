bba31b907b19e0b66c6f28b2c93750358b91ccef
Dephney <mmagotsheung@gmail.com>
2022-09-19 14:43:38 -0700

    add if statement

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,8 @@
 /**
  * print_array - a function that prints n elements of an array of integers
  * followed by a new line
- * @a - array to be printed
- * @n - the number of elements of the array to be printed
+ * @a: array to be printed
+ * @n: the number of elements of the array to be printed
  * Return: void
  */
 void print_array(int *a, int n)
@@ -14,8 +14,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		
-		if (i < n - 1)	
+		if (i < n - 1)
 			printf(", ");
 	}
 	printf("\n");
