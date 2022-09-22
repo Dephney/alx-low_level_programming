6d40434fa02c1c793fc48c5bb13cd8b105277cce
Dephney <mmagotsheung@gmail.com>
2022-09-22 09:36:43 -0700

    add header file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,27 +2,27 @@
 
 /**
  * *leet - a function that encodes a string into 1337
- * @p: pointer to string.
+ * @s: pointer to string.
  * Return: char
  */
-char *leet(char *p)
+char *leet(char *s)
 {
 	int i = 0, j = 0;
 
-	char *letters = "aAeEoOlLtT", *numbers = "4433001177";
+	string *letters = "aAeEoOlLtT", *numbers = "4433001177";
 
-	while (p[i] != '\0')
+	while (s[i] != '\0')
 	{
-		while (letter[j] != '\0')
+		while (letters[j] != '\0')
 		{
-			if (p[i] == letters[j])
-				p[i]  = numbers[j];
+			if (s[i] == letters[j])
+				s[i]  = numbers[j];
 			j++;
 		}
 		j = 0;
 		i++;
 	}
-	return (p);
+	return (s);
 }
 
 
