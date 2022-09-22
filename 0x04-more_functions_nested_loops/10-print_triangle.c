f586ad3e80cba7a5f8eb46d6a54c874d923c7c4c
Dephney <mmagotsheung@gmail.com>
2022-09-22 16:51:32 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,8 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -16,15 +15,15 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = i; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
-				_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
+				k = (size - i) - 1;
+				if (j < k)
+					_putchar(' ');
+				else
+					_putchar(35);
 			}
 
 			_putchar('\n');
