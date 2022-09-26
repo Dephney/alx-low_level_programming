ccc58dc7622889aa01a0a3d0ce6195b054a75638
Dephney <mmagotsheung@gmail.com>
2022-09-26 13:17:18 -0700

    correct indent

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,11 @@ char *_strchr(char *s, char c)
 	unsigned int i = 0;
 
 	while (s[i] != c && s[i] != '\n')
-	{
-		if (s[i] == c)
-			return (&s[i]);
-		else
-			return (NULL);
 		i++;
-	}
 
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
 
 }
